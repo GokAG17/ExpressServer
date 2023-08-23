98fa5068c7837ce5ce19636131125917bce5f4b5
Gokulraj <gokulraj790480@gmail.com>
2023-08-23 06:40:47 +0530

    update

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
